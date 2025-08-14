9cc1a047bd0fd70ace7321a4c54af59fc2f1ddb8
walterc98 <walter.condori.10@gmail.com>
2025-08-14 16:15:35 -0700

    2nd Commit: Adding book class

--- src/main/java/com/example/libraryapi/model/Books.java
+++ src/main/java/com/example/libraryapi/model/Books.java
@@ -1,4 +1,53 @@
 package com.example.libraryapi.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Books")
 public class Books {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Books() {
+
+    }
+    public Books(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
