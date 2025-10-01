8d1328f3649a0f95f8b773991ed799d09f4bd690
walterc98 <walter.condori.10@gmail.com>
2025-09-30 21:39:32 -0700

    added methods to BookService

--- src/main/java/com/example/libraryapi/service/BookService.java
+++ src/main/java/com/example/libraryapi/service/BookService.java
@@ -1,7 +1,42 @@
 package com.example.libraryapi.service;
 
+import com.example.libraryapi.model.Book;
+import com.example.libraryapi.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getbookById(Long id){
+        return bookRepository.findById(id);
+    }
+
+    public Book createBook(Book book){
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id){
+        if(bookRepository.existsById(id)){
+            bookRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("Book not found with id: " + id);
+        }
+    }
+
+
 }
